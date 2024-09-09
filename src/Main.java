8336aa0735ace98c9189244348c390a585af0501
Gavin Song <gavin.song@mail.utoronto.ca>
2024-09-09 09:55:09 -0400

    updated Main.java to work as FizzBuzz.java does

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // FizzBuzz.doFizzBuzz(30);
+        int i = 1;
+        while (i < 100) {
+            i = FizzBuzz.doFizzBuzz(i);
+        }
+    }
+}
